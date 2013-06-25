995a30e7b95644217472c0c6f942759e1683ef68
Jens Scherbl <jens@scherbl.net>
2013-06-25 04:26:38 +0200

    Code cleanup and JSLint compliance

--- assets/parenthesistabs.publish.js
+++ assets/parenthesistabs.publish.js
@@ -1,72 +1,129 @@
-(function($) {
+/*global window*/
 
-	Tabs = {
+(function (window, $) {
 
-		wrapper: null,
-		tabs: [],
-		storage: {
-			all: $([])
-		},
+    'use strict';
 
-		init: function() {
-			$('.field').each(Tabs.findTabs);
+    // module
 
-			if(Tabs.tabs.length) {
-				Tabs.createWrapper();
-				$.each(Tabs.tabs, Tabs.createTabs);
+    window.tabs = (function () {
 
-				// Events
-				Tabs.wrapper.on('click.parenthesistabs', 'li', Tabs.selectTab);
-				Tabs.wrapper.find('li:first').trigger('click.parenthesistabs');
-			}
-		},
+        // objects
 
-		findTabs: function() {
-			var field = $(this),
-				node = field.find('label').contents().get(0),
-				label = node.nodeValue.match(/\((.*?)\)/);
+        var tabs, fields, init, select;
 
-			if(label != null) {
-				var name = label[1];
+        // private api
 
-				// Store name
-				if($.inArray(name, Tabs.tabs) == -1) {
-					Tabs.tabs.push(name);
-					Tabs.storage[name] = $([]);
-				}
+        init = function () {
 
-				// Store field
-				Tabs.storage[name] = Tabs.storage[name].add(field);
-				Tabs.storage.all = Tabs.storage.all.add(field);
+            var wrapper;
 
-				// Remove name from field
-				node.nodeValue = node.nodeValue.replace(/\((.*?)\)/, '');
-			}
-		},
+            // init tabs
 
-		createWrapper: function() {
-			Tabs.wrapper = $('<ul />', {
-				class: 'tabs'
-			}).appendTo('#context');
-		},
+            tabs   = [];
 
-		createTabs: function() {
-			$('<li />', {
-				text: this
-			}).appendTo(Tabs.wrapper);
-		},
+            // init tabs
 
-		selectTab: function(event) {
-			var selected = $(this).addClass('selected').siblings().removeClass('selected').end(),
-				name = selected.text();
+            fields = {
 
-			Tabs.storage.all.hide();
-			Tabs.storage[name].show();
-		}
-	}
+                all : $([])
+            };
 
-	$(document).on('ready.metakeys', function(event) {
-		Tabs.init();
-	});
+            // find tabs and store fields
 
-})(window.jQuery);
+            $('.field').each(function () {
+
+                var field, node, label, name;
+
+                // get field
+
+                field = $(this);
+
+                // get field label
+
+                node  = field.find('label').contents().get(0);
+
+                // check field label
+
+                label = node.nodeValue.match(/\(([\w\W]*?)\)/);
+
+                // if field label has parenthesis
+
+                if (label) {
+
+                    // get tab name
+
+                    name = label[1];
+
+                    // store tab
+
+                    if (!fields[name]) {
+
+                        tabs.push(name);
+                        fields[name] = $([]);
+                    }
+
+                    // store field
+
+                    fields[name] = fields[name].add(field);
+                    fields.all   = fields.all.add(field);
+
+                    // remove parenthesis from field label
+
+                    node.nodeValue = node.nodeValue.replace(/\(([\w\W]*?)\)/, '');
+                }
+            });
+
+            // if tabs
+
+            if (tabs.length !== 0) {
+
+                // create wrapper
+
+                wrapper = $('<ul />', { 'class' : 'tabs' });
+
+                // create tabs
+
+                $(tabs).each(function (key, value) {
+
+                    $('<li />', { 'text' : value })
+                        .on('click', select)
+                        .appendTo(wrapper);
+                });
+
+                // append wrapper and select first tab
+
+                wrapper
+                    .appendTo('#context')
+                    .find('li:first')
+                    .trigger('click');
+            }
+        };
+
+        select = function () {
+
+            var tab = $(this);
+
+            tab.addClass('selected').siblings().removeClass('selected');
+
+            fields.all.hide();
+
+            fields[tab.text()].show();
+        };
+
+        // public api
+
+        return {
+
+            init   : init,
+            select : select
+        };
+
+    }());
+
+    $(window.document).on('ready.metakeys', function () {
+
+        window.tabs.init();
+    });
+
+}(window, window.jQuery));
