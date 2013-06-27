306a6acb8f8e1caf29e5406e2ab7a308b8719d00
Nils Hörrmann <post@nilshoerrmann.de>
2013-06-27 10:00:05 +0200

    Add keyboard navigation via tabs based on @jensscherbl's work. Make sure that complex fields with multiple inputs are considered.
    See https://github.com/hananils/parenthesistabs/commit/ecca887dde9dc86401f82bcf0d5ae6de0d31ed93

--- assets/parenthesistabs.publish.js
+++ assets/parenthesistabs.publish.js
@@ -2,6 +2,7 @@
 
 	Tabs = {
 
+		contents: null,
 		wrapper: null,
 		tabs: [],
 		storage: {
@@ -9,15 +10,18 @@
 		},
 
 		init: function() {
-			$('.field:visible').each(Tabs.findTabs);
+			Tabs.contents = $('#contents');
+			Tabs.contents.find('.field:visible').each(Tabs.findTabs);
 
 			if(Tabs.tabs.length) {
 				Tabs.createWrapper();
 				$.each(Tabs.tabs, Tabs.createTabs);
 
-				// Events
-				Tabs.wrapper.on('click.parenthesistabs', 'li', Tabs.selectTab);
-				Tabs.wrapper.find('li:first').trigger('click.parenthesistabs');
+				// Select tab
+				Tabs.wrapper.on('click.parenthesistabs', 'li', Tabs.selectTab).find('li:first').trigger('click.parenthesistabs');
+
+				// Switch tab
+				Tabs.contents.on('keydown.parenthesistabs', '.field:visible input, .field:visible textarea', Tabs.switchTab);
 			}
 		},
 
@@ -63,6 +67,21 @@
 
 			Tabs.storage.all.hide();
 			Tabs.storage[name].show();
+
+			// Focus first visible field
+			Tabs.contents, Tabs.contents.find('.field:visible:first input:first, .field:visible:first textarea:first').first().trigger('focus.parenthesistabs');
+		},
+
+		switchTab: function(event) {
+			var current = $(event.target),
+				field = current.parents('.field'),
+				lastInput = field.find('input, textarea').filter(':visible:last'),
+				lastField = Tabs.contents.find('.field:visible:has(input:visible, textarea:visible)').last();
+
+			if(event.which === 9 && current[0] == lastInput[0] && field[0] == lastField[0]) {
+				event.preventDefault();
+				Tabs.wrapper.find('.selected').next().trigger('click.parenthesistabs');
+			}
 		},
 
 		getTextNode: function(element, position) {
